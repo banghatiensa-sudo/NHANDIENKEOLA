c7b6fe6c8b338e6b6ecef9516226107fd658f38d
banghatiensa-sudo <banghatiensa@gmail.com>
2026-09-20 12:36:34 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/NHANDIENKEOLA/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
